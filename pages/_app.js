7dfa83f2b2229d54d8aedf399f87f0d1cd07789f
Divyam Prusty <divyamprusty536@gmail.com>
2021-12-31 19:05:47 +0530

    render loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import LoadingScreen from '../components/loadingScreen';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => setLoading(true), 6000);
+  })
+
+  return (
+    <>
+      {!loading ? (
+        <React.Fragment>
+          <Component{...pageProps} />
+        </React.Fragment>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
 }
 
 export default MyApp
